d83e0f2e4465ec2f8df732ac7ed2be6b055d7154
Rodrigo Bossini <professorbossini@gmail.com>
2023-03-29 12:04:47 -0300

    regras implementadas e metodo para obtencao de estado

--- Personagem.java
+++ Personagem.java
@@ -1,8 +1,8 @@
 public class Personagem{
   String nome;
-  int energia;
-  int fome;
-  int sono;
+  int energia = 10;
+  int fome = 0;
+  int sono = 0;
 
   //4 partes:
   //tipo de retorno
@@ -10,19 +10,49 @@ public class Personagem{
   //lista de parâmetros
   //corpo
   void cacar(){
-    System.out.printf("%s esta cacando...%n", nome);    
+    if(energia >= 2){
+      System.out.printf("%s esta cacando...%n", nome);  
+      energia = energia - 2; // energia -= 2;  
+    }
+    else{
+      System.out.printf("%s sem energia para cacar...%n", nome);
+    }
+    fome = Math.min(fome + 1, 10);
+    sono = Math.min(sono + 1, 10);
   }
 
   void comer(){
-    System.out.println(nome + " esta comendo...");
+    if(fome >= 1){      
+      System.out.println(nome + " esta comendo...");
+      energia = Math.min(energia + 1, 10);
+      fome -= 1; // fome = fome - 1;
+    }
+    else{
+      System.out.printf("%s sem fome...%n", nome);
+    }
   }
 
   void dormir(){
-    System.out.print(nome + " esta dormindo...\n");
+    if(sono >= 1){
+      System.out.print(nome + " esta dormindo...\n");
+      sono--; //--sono;
+      energia = energia + 1 <= 10 ? energia + 1: 10;
+    }
+    else{
+      System.out.printf("%s sem sono...%n", nome);
+    }
   }
 
   double calcularRaiz2(double n){
     double resultado = Math.sqrt(n);
     return resultado;
+  }
+
+  String obterEstado(){
+    String estado = String.format(
+      "energia: %d, fome: %d, sono: %d, nome: %s",
+      energia, fome, sono, nome
+    );
+    return estado;  
   }
 }
